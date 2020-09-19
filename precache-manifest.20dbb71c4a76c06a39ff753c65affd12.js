84146dc53769f411c5af16181658cdca476d163b
SteinarV-design <steinarvdesign@gmail.com>
2020-09-20 01:06:15 +0200

    Updates

---
+++ precache-manifest.20dbb71c4a76c06a39ff753c65affd12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c793da095f92c5f1896c6dd4b1cb2c",
+    "url": "/react-training-01/index.html"
+  },
+  {
+    "revision": "b434e9b679c5c8977312",
+    "url": "/react-training-01/static/css/main.2b49f64b.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/react-training-01/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-training-01/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b434e9b679c5c8977312",
+    "url": "/react-training-01/static/js/main.5468c064.chunk.js"
+  },
+  {
+    "revision": "5faae2fea5cbb12b4078",
+    "url": "/react-training-01/static/js/runtime-main.56bb5b27.js"
+  }
+]);
